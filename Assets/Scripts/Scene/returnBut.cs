2bed28da6c7d5ddecc9da5a0d25db29839e4e2fb
Lmonetti <sylviali1994@gmail.com>
2016-04-30 12:12:00 -0700

    Added replay video scene, where user can replay captured videos. Replay button and return button will bring user to different scenes.

---
+++ Assets/Scripts/Scene/returnBut.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class returnBut : MonoBehaviour {
+
+	// Use this for initialization
+	public void onMouseDown() {
+		Debug.Log("Mouse down");
+		Application.LoadLevel("main");
+	}
+}
